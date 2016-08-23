75bf780bf610bc13401d745cc2282c3ed3910c9f
Viktor Scheglov <viktor@scheglov.pro>
2016-08-23 12:24:32 +0300

    fix update render

--- src/components/news/index.js
+++ src/components/news/index.js
@@ -10,28 +10,32 @@ export default class NewsPage extends Component {
             data: {}
         }
     }
-    componentWillReceiveProps(nextProps) {
-      this.setState({
-        id: nextProps.params.id
-      })
-    }
-    componentWillMount() {
-        load('post/' + this.state.id).then((stories) => {
+
+    loadData = (e) => {
+        load(e).then((stories) => {
             this.setState({
                 data: stories.data[0]
             });
         });
     }
-  render() {
-  	let data = this.state.data;
-  	let image = this.state.data.image ? data.image['540x305'].replace(Config.CDN,Config.SITE) : '';
-    return (
-      <div className='n_article'>
-      <h1>{data.title}</h1>
-      <div dangerouslySetInnerHTML={{__html: data.description }} className='n_article__desc'/>
-      <div className="n_article__image"><img src={image} alt={data.title} title={data.description}/></div>
-      <div dangerouslySetInnerHTML={{__html: data.content }} className='n_article__content'/>
-      </div>
-    )
-  }
+    componentWillMount() {
+        this.loadData('post/' + this.state.id);
+    } 
+    componentWillReceiveProps(nextProps) {
+      if (nextProps.params.id !== this.props.params.id) {
+        this.loadData('post/' + nextProps.params.id);
+      }
+    }
+    render() {
+    	let data = this.state.data;
+    	let image = this.state.data.image ? data.image['540x305'].replace(Config.CDN,Config.SITE) : '';
+      return (
+        <div className='n_article'>
+        <h1>{data.title}</h1>
+        <div dangerouslySetInnerHTML={{__html: data.description }} className='n_article__desc'/>
+        <div className="n_article__image"><img src={image} alt={data.title} title={data.description}/></div>
+        <div dangerouslySetInnerHTML={{__html: data.content }} className='n_article__content'/>
+        </div>
+      )
+    }
 }
