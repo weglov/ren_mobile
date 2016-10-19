51737d76f63355a0e3d9ba9f3d7c4f92b662a568
Viktor Scheglov <viktor@scheglov.pro>
2016-10-19 10:42:29 +0300

    change files and sprite

--- src/components/live/live.js
+++ src/components/live/live.js
@@ -1,14 +1,6 @@
 import React from 'react';
 
 export default class Live extends React.Component {
-  static propTypes = {
-    name: React.PropTypes.string,
-  };
-
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="r__player">
